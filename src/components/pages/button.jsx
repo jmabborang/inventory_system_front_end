7dad84f3bb0764b5b3dea35bf30df7813a5e84fa
Jeffrey Mabborang <141696680+jmabborang@users.noreply.github.com>
2025-04-19 14:50:26 +0800

    Example Button Ui Setup

---
+++ src/components/pages/button.jsx
@@ -0,0 +1,36 @@
+import { Stack, Button, IconButton } from '@mui/material'
+import CheckIcon from '@mui/icons-material/Check';
+
+function ButtonUI() {
+	return (
+		<Stack spacing={2}>
+			<Stack spacing={2} direction='row'>
+				<Button variant='text' href='https://google.com'>Click</Button>
+				<Button variant='contained'>Click</Button>
+				<Button variant='outlined'>Click</Button>
+			</Stack>
+			<Stack spacing={2} direction='row'>
+				<Button variant="contained" color='primary'>Primary</Button>
+				<Button variant="contained" color='secondary'>Secondary</Button>
+				<Button variant="contained" color='error'>Error</Button>
+				<Button variant="contained" color='warning'>Warning</Button>
+				<Button variant="contained" color='info'>Info</Button>
+				<Button variant="contained" color='success'>Success</Button>
+			</Stack>
+			<Stack display='block' spacing={2} direction={'row'}>
+				<Button variant='contained' size='small'>small</Button>
+				<Button variant='contained' size='medium'>medium</Button>
+				<Button variant='contained' size='large'>large</Button>
+			</Stack>
+			<Stack display='block' spacing={2} direction={'row'}>
+				<Button variant='contained' startIcon={<CheckIcon />} disableRipple onClick={() => alert('Hello')}>Start</Button>
+				<Button variant='contained' endIcon={<CheckIcon />} disableElevation>End</Button>
+				<IconButton aria-label='send' color='success' size='small'>
+				  <CheckIcon/>
+				</IconButton>
+			</Stack>
+		</Stack>
+	)
+}
+
+export default ButtonUI
