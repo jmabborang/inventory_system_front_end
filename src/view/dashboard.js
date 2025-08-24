39896688198c93dbfa3abc0f100357d84b11ec9c
Jeffrey Mabborang <jeffrey.mabborang@jeonsoft.com>
2025-08-24 11:40:33 +0800

    Dashboard Chart

--- src/view/dashboard.js
+++ src/view/dashboard.js
@@ -175,7 +175,7 @@ export default function PrimarySearchAppBar() {
   const renderView = () => {
     switch (view) {
       case 'dashboard':
-        return <DashboardView/>;
+        return <><title>{view.toUpperCase()}</title><DashboardView/></>;
       case 'inventory':
         return <h2>Inventory View</h2>;
       case 'security':
@@ -275,8 +275,25 @@ export default function PrimarySearchAppBar() {
       </Box>
       <Box display="flex">
         <Navigation setView={setView} />
-        <Box flexGrow={1} p={3}>
-          {renderView()}
+        <Box sx={{ height: "100vh", bgcolor: "#fff" }} flexGrow={1}>
+          <Box mt={2} mr={2} mb={2} ml={2}>
+            <Breadcrumbs aria-label="breadcrumb" mb={2}>
+              <Link underline="hover" color="inherit" href="/dashboard">
+                Dashboard
+              </Link>
+              <Link
+                underline="hover"
+                color="inherit"
+                href="/material-ui/getting-started/installation/"
+              >
+                Core
+              </Link>
+              <Typography sx={{ color: 'text.primary' }}>Breadcrumbs</Typography>
+            </Breadcrumbs>
+            <div>
+              {renderView()}
+            </div> 
+          </Box>
         </Box>
     </Box>
     </>
